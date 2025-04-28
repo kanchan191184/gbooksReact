40f83b768d5e5cc8c8e77759e6773dbadaa7f400
kangarg <kanchan191184@gmail.com>
2025-04-28 18:28:14 +1000

    Fix the secret key/API code

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,2 +1,2 @@
-export const API_KEY = process.env.REACT_APP_API_KEY; // API key for Google Books API
-export const API_URL = process.env.REACT_APP_API_URL; // Base URL for Google Books API
+export const API_KEY = import.meta.env.VITE_API_KEY; // API key for Google Books API
+export const API_URL = import.meta.env.VITE_API_URL; // Base URL for Google Books API
